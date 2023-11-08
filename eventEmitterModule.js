bc86d7be126fd7fca25dc94900cf9fdc3c31adc1
Aljohanii <143590316+Aljohanii@users.noreply.github.com>
2023-11-08 15:18:40 +0300

    Delete eventEmitterModule.js

--- eventEmitterModule.js
+++
@@ -1,7 +0,0 @@
-const EventEmitter = require('events');
-
-class CustomEmitter extends EventEmitter {}
-
-const customEmitterInstance = new CustomEmitter();
-
-module.exports = customEmitterInstance;
